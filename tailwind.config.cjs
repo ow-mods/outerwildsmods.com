1340cef33500abc236cba9cf2189bd72f397fe3d
Raicuparta <raicuparta@gmail.com>
2021-12-07 18:10:00 +0100

    Lighten

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,7 @@ const config = {
 				white: '#ffffff',
 				black: '#000000',
 				accent: '#df8460',
+				'accent-light': '#f0c6b5',
 				cta: '#35823f',
 				background: '#1f2122',
 				light: 'rgba(255, 255, 255, 0.65)',
