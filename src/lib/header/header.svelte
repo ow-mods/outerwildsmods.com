07b621a55d3dd25220557264b92f256d6b448cc9
Raicuparta <raicuparta@gmail.com>
2021-12-08 11:31:13 +0100

    recreate header for renaming

---
+++ src/lib/header/header.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import GithubCorner from '$lib/github-corner/github-corner.svelte';
+	import Navigation from '$lib/navigation/navigation.svelte';
+	import SmartLink from '$lib/smart-link/smart-link.svelte';
+	import Stars from '$lib/stars/stars.svelte';
+</script>
+
+<header class="relative text-center">
+	<GithubCorner />
+	<Stars />
+	<h1 class="pt-4 pb-5 text-4xl">
+		<SmartLink class="font-thin text-white" href="/">Outer Wilds Mods</SmartLink>
+	</h1>
+	<Navigation />
+</header>
