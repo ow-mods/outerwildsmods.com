d17f015cde26e3e8b3c1a31b2641be789066237c
Raicuparta <raicuparta@gmail.com>
2021-12-08 12:10:31 +0100

    Button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,7 @@ const config = {
 				accent: '#df8460',
 				'accent-light': '#f0c6b5',
 				cta: '#35823f',
+				'cta-dark': '#27632f',
 				background: '#1f2122',
 				light: 'rgba(255, 255, 255, 0.65)',
 				dark: '#191a1b'
