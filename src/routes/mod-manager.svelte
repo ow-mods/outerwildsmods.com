411bbb1c9e40a29c2d1df6ecc246078a521c775b
Raicuparta <raicuparta@gmail.com>
2021-12-08 14:12:21 +0100

    Prerender everything

--- src/routes/mod-manager.svelte
+++ src/routes/mod-manager.svelte
@@ -1,3 +1,7 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
 <script lang="ts">
 	import LinkButton from '$lib/link-button.svelte';
 
--- src/routes/outer-wilds-alpha.svelte
+++ src/routes/outer-wilds-alpha.svelte
@@ -1,1 +1,5 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
 Alpha
