1eb0b3945074992cfa00fd7f4f23a67825e2e548
Raicuparta <raicuparta@gmail.com>
2021-12-08 11:27:09 +0100

    breakpoints

---
+++ src/lib/page-layout/page-layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	let className = '';
+	export { className as class };
+	export let isWide = false;
+</script>
+
+<div class={twMerge(`max-w-screen-${isWide ? 'md' : 'sm'} p-4 mx-auto`, className)}>
+	<slot />
+</div>
