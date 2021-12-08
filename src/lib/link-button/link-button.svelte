a906b3b33217e8f02283e22e484687618798f048
Raicuparta <raicuparta@gmail.com>
2021-12-08 12:10:31 +0100

    Button

--- src/lib/link-button/link-button.svelte
+++ src/lib/link-button/link-button.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
 	import type { ButtonVariant } from './button-variant';
 
-	import { Wrapper, Content } from './link-button.styles';
 	export let variant: ButtonVariant = 'secondary';
 	export let isExternal = false;
 	export let href: string | undefined = undefined;
 	export let rel: string | undefined = undefined;
+
+	const backgroundColor = variant === 'secondary' ? 'accent' : 'cta';
+	const textColor = variant === 'secondary' ? 'dark' : 'white';
 </script>
 
 <a
-	class={Wrapper(variant)}
+	class={`
+		block
+		rounded
+		bg-${backgroundColor}
+		text-${textColor}
+		p-1
+		font-bold
+		mx-auto
+		border-4
+		border-transparent
+		hover:border-cta-dark
+		active:bg-cta-dark
+	`}
 	{href}
 	target={isExternal ? '_blank' : undefined}
 	rel={rel ?? (isExternal ? 'noopener noreferrer' : undefined)}
 >
-	<div class={Content}><slot /></div>
+	<div class="h-full w-full pointer-events-none text-center"><slot /></div>
 </a>
