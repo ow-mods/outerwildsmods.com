d287553ffb008a64e90d77b8f2bdd1e83cc85b7b
Raicuparta <raicuparta@gmail.com>
2021-12-08 13:24:13 +0100

    page section

--- src/lib/page-section/page-section-title.svelte
+++ src/lib/page-section/page-section-title.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import HrLine from '$lib/hr-line.svelte';
-	import { TitleWrapper, Title } from './page-section.styles';
 
 	export let id: string;
 </script>
 
-<div class={TitleWrapper}>
+<div class="flex items-end mb-4">
 	<HrLine />
-	<h2 class={Title} {id}><slot /></h2>
+	<h2 class="text-2xl" {id}><slot /></h2>
 	<HrLine />
 </div>
