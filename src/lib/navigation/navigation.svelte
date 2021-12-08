a0b8156bc236c1d3720e878428941f881f7b21b4
Raicuparta <raicuparta@gmail.com>
2021-12-08 12:42:05 +0100

    navigation

--- src/lib/navigation/navigation.svelte
+++ src/lib/navigation/navigation.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import PageLayout from '$lib/page-layout.svelte';
 	import NavigationLink from './navigation-link.svelte';
-	import { Wrapper, NavWrapper, HiddenInput, MenuButton } from './navigation.styles';
 </script>
 
 <PageLayout class="p-0">
 	<!-- <MenuButton htmlFor="navigation-menu-button">☰</MenuButton> -->
 	<!-- <HiddenInput type="checkbox" id="navigation-menu-button" /> -->
-	<nav class={NavWrapper}>
+	<nav class="flex flex-col sm:flex-row">
 		<NavigationLink href="/">Home</NavigationLink>
 		<NavigationLink href="/mod-manager">Manager</NavigationLink>
 		<NavigationLink href="/mods">Mods</NavigationLink>
