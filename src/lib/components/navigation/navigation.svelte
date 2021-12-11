a59972ed24decb560c28bd0ab12e6e4ce7c250c3
Raicuparta <raicuparta@gmail.com>
2021-12-11 19:32:01 +0100

    Cleanup

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -5,7 +5,7 @@
 <div class="max-w-screen-sm mx-auto px-4">
 	<!-- <MenuButton htmlFor="navigation-menu-button">☰</MenuButton> -->
 	<!-- <HiddenInput type="checkbox" id="navigation-menu-button" /> -->
-	<nav class="flex flex-col sm:flex-row gap-4">
+	<nav class="flex flex-col sm:flex-row sm:gap-4">
 		<NavigationLink href="/">Home</NavigationLink>
 		<NavigationLink href="/mod-manager">Mod Manager</NavigationLink>
 		<NavigationLink href="/mods">Mods</NavigationLink>
