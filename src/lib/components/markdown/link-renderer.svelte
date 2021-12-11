c26c0fd2f368637ab5e926f68812277f3ee1c614
Raicuparta <raicuparta@gmail.com>
2021-12-11 19:01:20 +0100

    Cleanup link-renderer

--- src/lib/components/markdown/link-renderer.svelte
+++ src/lib/components/markdown/link-renderer.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-	import { twMerge } from 'tailwind-merge';
-
 	export let href: string;
-	export let isExternal = false;
-	export let rel: string | undefined = undefined;
-	export let prefetch: true | undefined = undefined;
-	let className = '';
-	export { className as class };
 </script>
 
-<a
-	{href}
-	class={twMerge('text-accent hover:text-accent-light', className)}
-	target={isExternal ? '_blank' : undefined}
-	rel={rel ?? (isExternal ? 'noopener noreferrer' : undefined)}
-	sveltekit:prefetch={prefetch}
->
+<a {href} class="link" rel="noopener noreferrer">
 	<slot />
 </a>
