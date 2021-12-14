5b95e313a0e15e2579ca4d743b5da2c41e43275f
Raicuparta <raicuparta@gmail.com>
2021-12-14 16:24:02 +0100

    refactor error message

---
+++ src/lib/components/error-message.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import PageSectionImage from '$lib/components/page-section/page-section-image.svelte';
+	import PageSection from '$lib/components/page-section/page-section.svelte';
+	import PageSectionColumns from '$lib/components/page-section/page-section-columns.svelte';
+	import PageSectionDescription from '$lib/components/page-section/page-section-description.svelte';
+</script>
+
+
+<PageSection id="not-found" title="Page not found">
+	<PageSectionColumns>
+		<PageSectionDescription
+			description="Sorry. Not sure what you were looking for but I didn't find it. Try using the navigation buttons at the top of the page to find what you want."
+		/>
+		<PageSectionImage title="Error" imageUrl="/images/error.png" height={300} />
+	</PageSectionColumns>
+</PageSection>
--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -13,24 +13,14 @@
 
 <script lang="ts">
 	import PageLayout from '$lib/components/page-layout.svelte';
-	import PageSectionImage from '$lib/components/page-section/page-section-image.svelte';
-	import PageSection from '$lib/components/page-section/page-section.svelte';
-	import PageSectionColumns from '$lib/components/page-section/page-section-columns.svelte';
-	import PageSectionDescription from '$lib/components/page-section/page-section-description.svelte';
+	import ErrorMessage from '$lib/components/error-message.svelte';
 
 	export let message = '';
 	export let stack = '';
 </script>
 
 <PageLayout>
-	<PageSection id="not-found" title="Page not found">
-		<PageSectionColumns>
-			<PageSectionDescription
-				description="Sorry. Not sure what you were looking for but I didn't find it. Try using the navigation buttons at the top of the page to find what you want."
-			/>
-			<PageSectionImage title="Error" imageUrl="/images/error.png" height={300} />
-		</PageSectionColumns>
-	</PageSection>
+	<ErrorMessage />
 	<code class="whitespace-pre-wrap break-words">{message}</code>
 	<div class="bg-dark rounded px-2">
 		<pre class="whitespace-pre-wrap break-words">
