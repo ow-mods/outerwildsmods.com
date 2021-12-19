23d6fbd32c14d3266928178c4c6d4bebcb74c031
Ricardo <Raicuparta@users.noreply.github.com>
2021-12-19 17:08:16 +0000

    add post-build script with sitemap and 404

---
+++ post-build.mjs
@@ -0,0 +1,26 @@
+import { writeFileSync, renameSync } from 'fs';
+import { globby } from 'globby';
+
+const urlBase = 'https://outerwildsmods.com';
+
+const generateSitemap = async () => {
+	const pages = await globby(['build/**/*.html', '!build/404']);
+
+	const pageUrls = pages
+		.map((page) => {
+			const path = `${urlBase}/${page.replace(/(index|.html|build\/)/gm, '')}`;
+			return `\n  <url><loc>${path}</loc></url>`;
+		})
+		.join('');
+
+	const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${pageUrls}\n</urlset>`;
+
+	writeFileSync('build/sitemap.xml', sitemap);
+};
+
+const move404 = () => {
+	renameSync('build/404/index.html', 'build/404.html');
+};
+
+generateSitemap();
+move404();
