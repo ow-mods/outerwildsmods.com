890f6fd1d8ea42f31be2a068a9d9790a95973641
Raicuparta <raicuparta@gmail.com>
2021-12-29 23:47:29 +0100

    Listing repos

--- src/lib/octokit.d.ts
+++ src/lib/octokit.d.ts
@@ -1,3 +1,10 @@
-import { Octokit } from 'octokit';
+import type { Octokit as OctokitInstance } from 'octokit';
+import { Octokit as OctokitClass } from 'octokit';
 
-export type Octokit = typeof Octokit;
+export type Octokit = typeof OctokitClass;
+export type OctokitAuthenticatedUser = Awaited<
+	ReturnType<OctokitInstance['rest']['users']['getAuthenticated']>
+>['data'];
+export type OctokitRepoArray = Awaited<
+	ReturnType<OctokitInstance['rest']['repos']['listForAuthenticatedUser']>
+>['data'];
--- src/routes/upload-mod.svelte
+++ src/routes/upload-mod.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import PageLayout from '$lib/components/page-layout.svelte';
-	import type { Octokit } from '$lib/octokit';
+	import type { Octokit, OctokitAuthenticatedUser, OctokitRepoArray } from '$lib/octokit';
 
 	import { onDestroy, onMount } from 'svelte';
+	import A4 from './404.svelte';
 
 	let Octokit: Octokit | undefined;
+	let user: OctokitAuthenticatedUser;
+	let errorMessage: string;
+	let modRepos: OctokitRepoArray = [];
 
-	let githubToken = '';
+	let githubToken = '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';
 	let interval: NodeJS.Timer;
 
 	onMount(() => {
@@ -28,11 +32,34 @@
 		}
 	});
 
-	const handleClickAuthenticate = () => {
+	const handleClickAuthenticate = async () => {
 		if (!Octokit) return;
 
 		const octokit = new Octokit({ auth: githubToken });
-		console.log('octokit', octokit);
+
+		try {
+			user = (await octokit.rest.users.getAuthenticated()).data;
+
+			const repos = await octokit.paginate(octokit.rest.repos.listForAuthenticatedUser, {
+				per_page: 100,
+			});
+
+			// TODO deal with the type.
+			modRepos = repos.filter((repo) => repo.topics?.includes('outer-wilds'));
+
+			console.log('repos', JSON.stringify(modRepos));
+
+			// modRepos = repos.filter(repo => repo.template_repository.)
+
+			// const a = await octokit.rest.repos.createUsingTemplate({
+			// 	template_owner: 'Raicuparta',
+			// 	template_repo: 'ow-mod-template',
+			// 	name: 'ow-new-mod-test',
+			// 	private: true,
+			// });
+		} catch (error) {
+			errorMessage = `Error authenticating: ${error}`;
+		}
 	};
 </script>
 
@@ -45,6 +72,16 @@
 </svelte:head>
 
 <PageLayout>
+	<p>
+		{#if user}
+			Authenticated as
+			<a class="link" href={user.html_url}>
+				{user.login}
+			</a>
+		{:else}
+			Please authenticate
+		{/if}
+	</p>
 	<div class="flex w-full gap-4">
 		<input
 			class="flex-1 rounded bg-dark py-1 px-2 block"
@@ -53,4 +90,19 @@
 		/>
 		<button class="button link bg-dark" on:click={handleClickAuthenticate}> Authenticate </button>
 	</div>
+	{#if errorMessage}
+		<p>
+			{errorMessage}
+		</p>
+	{/if}
+	{#if modRepos.length > 0}
+		<h2>Select a repo</h2>
+		<div class="flex flex-col gap-2">
+			{#each modRepos as repo (repo.id)}
+				<div class="bg-dark rounded p-2">
+					{repo.name}
+				</div>
+			{/each}
+		</div>
+	{/if}
 </PageLayout>
