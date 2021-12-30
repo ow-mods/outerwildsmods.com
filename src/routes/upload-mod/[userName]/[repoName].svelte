6d35d804a45c02c87a0a813a911e0eee31c10d7a
Raicuparta <raicuparta@gmail.com>
2021-12-30 14:57:10 +0100

    Uploading that does't actually work :(

--- src/routes/upload-mod/[userName]/[repoName].svelte
+++ src/routes/upload-mod/[userName]/[repoName].svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { OctokitRelease, OctokitRepo } from '$lib/octokit';
-	import { octokitStore } from '$lib/store';
+	import { githubUserStore, octokitStore } from '$lib/store';
 	import { parse as parseSemver, stringify as stringifySemver } from 'semver-utils';
 
 	let repo: OctokitRepo | undefined;
 	let file: File | undefined;
 	let release: OctokitRelease | undefined;
-	let currentVersion = '';
-	let nextVersion = '';
+	let currentVersion = '0.0.0';
+	let nextVersion = '1.0.0';
+
+	const repoParameters = {
+		owner: $page.params.userName,
+		repo: $page.params.repoName,
+	};
 
 	$: (async () => {
-		const repoParameters = {
-			owner: $page.params.userName,
-			repo: $page.params.repoName,
-		};
+		if (repo) return;
 
 		let repoResponse = await $octokitStore?.rest.repos.get(repoParameters);
 		repo = repoResponse?.data;
@@ -34,20 +36,101 @@
 		}
 	})();
 
+	// TODO type if possible.
 	const handleFilesChange = (event: any) => {
 		if (!event.target?.files) return;
-
 		const droppedFile: File = event.target.files[0];
-
-		console.log('type', droppedFile.type);
 		if (!droppedFile.name.endsWith('.zip')) return;
-
 		file = event.target.files[0];
+	};
+
+	const handleUploadClick = async () => {
+		if (!file || !$octokitStore) return;
+
+		const releaseName = `${repoParameters.repo}-${nextVersion}`;
+
+		let foundRelease: OctokitRelease | undefined;
+
+		try {
+			foundRelease = (
+				await $octokitStore.rest.repos.getReleaseByTag({
+					...repoParameters,
+					tag: nextVersion,
+				})
+			)?.data;
+		} catch {
+			// TODO handle.
+		}
+
+		const release =
+			foundRelease ||
+			(
+				await $octokitStore.rest.repos.createRelease({
+					...repoParameters,
+					tag_name: nextVersion,
+					draft: false,
+					name: releaseName,
+				})
+			)?.data;
+
+		if (!release) return; // TODO error
+
+		const fileText = await file.text();
+
+		console.log('filetext', fileText);
+
+		console.log(
+			'${release.upload_url}?name=${releaseName}.zip',
+			`${release.upload_url}?name=${releaseName}.zip`
+		);
+		console.log('release.upload_url', release.upload_url);
+
+		// fetch(`${release.upload_url.replace('{?name,label}', '')}?name=${releaseName}.zip`, {
+		// 	headers: {
+		// 		accept: 'application/vnd.github.v3.raw',
+		// 		'accept-language': 'en-US,en;q=0.9',
+		// 		authorization: 'token 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',
+		// 		'content-type': 'application/json; charset=utf-8',
+		// 		'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+		// 		'sec-ch-ua-mobile': '?0',
+		// 		'sec-ch-ua-platform': '"Windows"',
+		// 		'sec-fetch-dest': 'empty',
+		// 		'sec-fetch-mode': 'no-cors',
+		// 		'sec-fetch-site': 'cross-site',
+		// 	},
+		// 	referrer: 'http://localhost:3000/',
+		// 	referrerPolicy: 'strict-origin-when-cross-origin',
+		// 	body: fileText,
+		// 	method: 'POST',
+		// 	mode: 'cors',
+		// 	credentials: 'include',
+		// });
+
+		if (repo) {
+			$octokitStore.rest.pulls.create({
+				...repoParameters,
+				base: repo.default_branch,
+				head: repo.default_branch,
+				body: 'hello',
+			});
+		}
+
+		// $octokitStore.rest.repos.uploadReleaseAsset({
+		// 	...repoParameters,
+		// 	release_id: release.id,
+		// 	data: fileText,
+		// 	mediaType: {
+		// 		format: 'raw',
+		// 	},
+		// 	name: `${releaseName}.zip`,
+		// });
 	};
 </script>
 
-{#if repo && release}
-	<h2>{repo.name}</h2>
+{#if repo}
+	<a href={repo.html_url}>
+		<h2 class="link">{repo.name}</h2>
+	</a>
 	<div class="flex justify-between">
 		<p>
 			Latest release: {currentVersion}
@@ -57,10 +140,7 @@
 		</p>
 	</div>
 
-	<div class="link relative bg-dark rounded p-4 h-48">
-		<!-- <label for="upload-input">
-			Drag and drop a zip file to the box below, or click it to open the file explorer.
-		</label> -->
+	<div class="link relative bg-dark border-2 border-dashed rounded-lg p-4 h-48">
 		<div class="flex flex-col justify-center items-center h-full">
 			{#if file}
 				{file.name}
@@ -76,7 +156,7 @@
 			on:change={handleFilesChange}
 		/>
 	</div>
-	<button />
+	<button on:click={handleUploadClick} class="button link bg-dark mt-4 w-full">Upload</button>
 {:else}
 	<p>
 		Please authenticate with an access token that has access to {$page.params.userName}/{$page
