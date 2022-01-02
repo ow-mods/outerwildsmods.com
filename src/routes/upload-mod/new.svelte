ab5453c058a0d181bc04ffd7a5ce55dabc49cb00
Raicuparta <raicuparta@gmail.com>
2022-01-02 16:23:33 +0100

    cleanup

--- src/routes/upload-mod/new.svelte
+++ src/routes/upload-mod/new.svelte
@@ -30,7 +30,7 @@
 		});
 
 		// Typed as any because the types for this are useless.
-		const manifest: any = (
+		const manifestResponse: any = (
 			await $octokitStore.rest.repos.getContent({
 				repo: repoName,
 				owner: repo.owner.login,
@@ -38,22 +38,21 @@
 			})
 		).data;
 
+		const manifest = JSON.parse(atob(manifestResponse.content));
+
 		await $octokitStore.rest.repos.createOrUpdateFileContents({
 			repo: repoName,
 			owner: repo.owner.login,
-			path: manifest.path,
-			sha: manifest.sha,
+			path: manifestResponse.path,
+			sha: manifestResponse.sha,
 			content: btoa(
 				JSON.stringify(
 					{
-						// TODO don't hardcode all this, just base it on the template.
-						filename: 'NewHorizonsConfig.dll',
+						...manifest,
 						author: repo.owner.login,
 						name: modName,
 						uniqueName: `${repo.owner.login}.${repoName}`,
 						version: '0.1.0',
-						owmlVersion: '2.1.0',
-						dependencies: ['xen.NewHorizons'],
 					},
 					null,
 					2
@@ -62,7 +61,7 @@
 			message: 'Update manifest.json',
 		});
 
-		goto(`upload-mod/${repo.full_name}`);
+		goto(`/upload-mod/${repo.full_name}`);
 	};
 </script>
 
