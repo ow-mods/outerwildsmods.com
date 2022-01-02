c3b6315d129dffe0a50b4d3702e327f773aac9ef
Raicuparta <raicuparta@gmail.com>
2022-01-02 19:31:24 +0100

    only show mods if signed in

--- src/routes/upload-mod/index.svelte
+++ src/routes/upload-mod/index.svelte
@@ -46,20 +46,20 @@
 {#if $githubUserStore}
 	<p class="text-xl mt-0">Start with a new mod</p>
 	<LinkButton href="/upload-mod/new">Create new mod</LinkButton>
-{/if}
-<p class="text-xl">Select a mod to edit</p>
-{#if loadingRepos}
-	<div class="outline outline-4 outline-dark rounded p-2 text-center">Loading mod list...</div>
-{:else if modRepos.length === 0}
-	<div class="outline outline-4 outline-dark rounded p-2 text-center">No planet mods found.</div>
-{:else}
-	<div class="flex flex-col gap-2">
-		{#each modRepos as repo (repo.id)}
-			<LinkButton href="upload-mod/{repo.owner.login}/{repo.name}">
-				{repo.name}
-			</LinkButton>
-		{/each}
-	</div>
+	<p class="text-xl">Select a mod to edit</p>
+	{#if loadingRepos}
+		<div class="outline outline-4 outline-dark rounded p-2 text-center">Loading mod list...</div>
+	{:else if modRepos.length === 0}
+		<div class="outline outline-4 outline-dark rounded p-2 text-center">No planet mods found.</div>
+	{:else}
+		<div class="flex flex-col gap-2">
+			{#each modRepos as repo (repo.id)}
+				<LinkButton href="upload-mod/{repo.owner.login}/{repo.name}">
+					{repo.name}
+				</LinkButton>
+			{/each}
+		</div>
+	{/if}
 {/if}
 {#if errorMessage}
 	<p>
