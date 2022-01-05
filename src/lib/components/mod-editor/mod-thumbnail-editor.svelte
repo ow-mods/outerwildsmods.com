f2d47d98dd2ca13eed15143657a531e784d06b69
Raicuparta <raicuparta@gmail.com>
2022-01-05 16:02:26 +0100

    Image uploading status

--- src/lib/components/mod-editor/mod-thumbnail-editor.svelte
+++ src/lib/components/mod-editor/mod-thumbnail-editor.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import { getBase64File } from '$lib/helpers/get-base-64-file';
-
 	import { octokit } from '$lib/store';
-	import type { Octokit } from 'octokit';
-
 	import type { RepoParameters } from 'src/routes/custom-worlds/create/[userName]/[repoName].svelte';
 
 	export let repoParameters: RepoParameters;
+
+	let isUploading = false;
 
 	const handleFileInputChange: svelte.JSX.FormEventHandler<HTMLInputElement> = async (event) => {
 		const file = event?.currentTarget.files?.[0];
@@ -16,40 +15,47 @@
 		let existingImage: any;
 
 		try {
-			existingImage = (
-				await $octokit.rest.repos.getContent({
-					...repoParameters,
-					path: file.name,
-				})
-			).data;
-		} catch {
-			console.log('existing image not found');
+			isUploading = true;
+
+			try {
+				existingImage = (
+					await $octokit.rest.repos.getContent({
+						...repoParameters,
+						path: file.name,
+					})
+				).data;
+			} catch {
+				existingImage = null;
+			}
+
+			const thumbnailResponse = await $octokit.rest.repos.createOrUpdateFileContents({
+				...repoParameters,
+				path: file.name,
+				content: await getBase64File(file),
+				message: 'Upload thumbnail',
+				sha: existingImage?.sha,
+			});
+
+			const readmeResponse = (await $octokit.rest.repos.getReadme(repoParameters)).data;
+			const readmeContent = atob(readmeResponse.content);
+			const newReadme = readmeContent.replace(
+				/(!\[.*?\])\(.+?\)/,
+				`$1(${thumbnailResponse.data.content?.download_url})`
+			);
+
+			await $octokit.rest.repos.createOrUpdateFileContents({
+				...repoParameters,
+				path: readmeResponse.path,
+				sha: readmeResponse.sha,
+				content: btoa(newReadme),
+				message: 'Update thumbnail',
+			});
+		} catch (error) {
+			// TODO handle errors
+			console.error(`Error uploading image: ${error}`);
+		} finally {
+			isUploading = false;
 		}
-
-		const thumbnailResponse = await $octokit.rest.repos.createOrUpdateFileContents({
-			...repoParameters,
-			path: file.name,
-			content: await getBase64File(file),
-			message: 'Upload thumbnail',
-			sha: existingImage?.sha,
-		});
-
-		console.log('thumbnail', thumbnailResponse);
-
-		const readmeResponse = (await $octokit.rest.repos.getReadme(repoParameters)).data;
-		const readmeContent = atob(readmeResponse.content);
-		const newReadme = readmeContent.replace(
-			/(!\[.*?\])\(.+?\)/,
-			`$1(${thumbnailResponse.data.content?.download_url})`
-		);
-
-		await $octokit.rest.repos.createOrUpdateFileContents({
-			...repoParameters,
-			path: readmeResponse.path,
-			sha: readmeResponse.sha,
-			content: btoa(newReadme),
-			message: 'Update thumbnail',
-		});
 	};
 </script>
 
@@ -61,8 +67,12 @@
 	disabled={!Boolean('TODO')}
 	on:change={handleFileInputChange}
 />
-<span
+<button
 	class="absolute bottom-0 right-0 bg-dark p-1 rounded-tl opacity-75 text-sm text-white pointer-events-none"
 >
-	✏️
-</span>
+	{#if isUploading}
+		Uploading...
+	{:else}
+		✏️
+	{/if}
+</button>
