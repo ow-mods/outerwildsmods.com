284d839f55f9eec160dd0e5575da0a8572b985b4
Ricardo <Raicuparta@users.noreply.github.com>
2022-02-22 13:09:04 +0100

    Smaller mod manager section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -86,14 +86,19 @@
 
 <PageLayout>
 	<PageSection title="Outer Wilds Mod Manager" id="mod-manager" isNarrow>
-		<PageSectionColumns>
-			<PageSectionImage imageUrl="/images/mod-manager-small.png" title="Outer Wilds Mod Manager" />
-			<PageSectionDescription
-				description="Use the Outer Wilds Mod Manager for downloading, installing, and managing mods."
-			/>
-		</PageSectionColumns>
-		<div class="mt-4">
-			<CtaButton href="/mod-manager">Outer Wilds Mod Manager</CtaButton>
+		<div class="flex flex-col md:flex-row gap-8">
+			<div>
+				<PageSectionImage
+					imageUrl="/images/mod-manager-small.png"
+					title="Outer Wilds Mod Manager"
+				/>
+			</div>
+			<div class="w-72 flex flex-col content-between">
+				<PageSectionDescription
+					description="Use the Outer Wilds Mod Manager for downloading, installing, and managing mods."
+				/>
+				<CtaButton href="/mod-manager">Outer Wilds Mod Manager</CtaButton>
+			</div>
 		</div>
 	</PageSection>
 	<PageSection title="Hot Mods" id="hotMods" moreHref="mods">
