bc3f373a2886f4868a4278cad313f9bee43400c7
Ricardo <Raicuparta@users.noreply.github.com>
2022-02-22 02:26:05 +0100

    Refactored mod sorting into separate file

--- src/lib/components/sorted-mod-grid.svelte
+++ src/lib/components/sorted-mod-grid.svelte
@@ -2,56 +2,11 @@
 	import ModCard from '$lib/components/card-grid/mod-card.svelte';
 	import CardGrid from '$lib/components/card-grid/card-grid.svelte';
 	import type { ModsRequestItem } from '../../routes/api/mods.json';
+	import { SortOrder, sortOrders } from '$lib/helpers/mod-sorting';
 
 	export let mods: ModsRequestItem[] = [];
 
-	const referenceScoreMilliseconds = 6 * 30 * 24 * 60 * 60 * 1000;
-	const downloadRatioNeededToBeHotter = 2;
-	const minScoreDate = new Date('2021-05-01').valueOf();
-	const calculateScore = (mod: ModsRequestItem) => {
-		const time = Date.now() - Math.max(0, new Date(mod.firstReleaseDate).valueOf() - minScoreDate);
-
-		return (
-			Math.log10(mod.downloadCount) / Math.log10(downloadRatioNeededToBeHotter) -
-			time / referenceScoreMilliseconds
-		);
-	};
-
-	const sortOrders = {
-		hot: {
-			title: 'Hot',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) => {
-				return calculateScore(modB) - calculateScore(modA);
-			},
-		},
-		mostDownloaded: {
-			title: 'Most downloaded',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) =>
-				modB.downloadCount - modA.downloadCount,
-		},
-		leastDownloaded: {
-			title: 'Least downloaded',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) =>
-				modA.downloadCount - modB.downloadCount,
-		},
-		newest: {
-			title: 'Newest',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) =>
-				new Date(modB.firstReleaseDate).valueOf() - new Date(modA.firstReleaseDate).valueOf(),
-		},
-		oldest: {
-			title: 'Oldest',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) =>
-				new Date(modA.firstReleaseDate).valueOf() - new Date(modB.firstReleaseDate).valueOf(),
-		},
-		updated: {
-			title: 'Recently updated',
-			compareFunction: (modA: ModsRequestItem, modB: ModsRequestItem) =>
-				new Date(modB.latestReleaseDate).valueOf() - new Date(modA.latestReleaseDate).valueOf(),
-		},
-	};
-
-	let sort: keyof typeof sortOrders = 'hot';
+	let sort: SortOrder = 'hot';
 
 	$: {
 		mods = mods.sort(sortOrders[sort].compareFunction);
