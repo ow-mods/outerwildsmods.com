4e2fb31fca22d5766b1912b0e09c73b79e6a8c74
Raicuparta <raicuparta@gmail.com>
2022-02-25 00:29:33 +0100

    resize text if too big

--- src/lib/components/card-grid/mod-card-details.svelte
+++ src/lib/components/card-grid/mod-card-details.svelte
@@ -4,8 +4,8 @@
 	export let mod: Pick<ModsRequestItem, 'name'>;
 </script>
 
-<div class="p-3">
-	<span class="text-accent font-normal">{mod.name}</span>
+<div class="p-3 overflow-ellipsis overflow-hidden">
+	<span class:text-xs={mod.name.length > 30} class:text-sm={mod.name.length > 20} class="text-accent font-normal whitespace-nowrap">{mod.name}</span>
 	<div class="text-light text-sm font-light">
 		<slot />
 	</div>
