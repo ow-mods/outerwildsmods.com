995813dc5862a575bded2bf3251d2749fcfede44
Raicuparta <raicuparta@gmail.com>
2022-02-25 16:53:30 +0100

    Consistent mod name size

--- src/lib/components/card-grid/mod-card-details.svelte
+++ src/lib/components/card-grid/mod-card-details.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="p-3 overflow-ellipsis overflow-hidden">
-	<span class:text-xs={mod.name.length > 30} class:text-sm={mod.name.length > 20} class="text-accent font-normal whitespace-nowrap">{mod.name}</span>
+	<span class="text-accent text-sm whitespace-nowrap">{mod.name}</span>
 	<div class="text-light text-sm font-light">
 		<slot />
 	</div>
