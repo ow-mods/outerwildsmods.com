3fcf42f38b8ffb9d5b4069ae8ac0716e8187ff92
Ricardo <Raicuparta@users.noreply.github.com>
2022-04-01 14:55:31 +0200

    better chart

--- src/routes/mods/[mod]/downloads.svelte
+++ src/routes/mods/[mod]/downloads.svelte
@@ -130,72 +130,110 @@
 		x: 0,
 		y: 0,
 	};
+
+	const getDateText = (historyPoint: HistoryPoint) => {
+		const date = new Date(historyPoint.UnixTimestamp * 1000);
+
+		return [
+			date.toLocaleDateString(undefined, {
+				day: '2-digit',
+				month: 'short',
+				year: 'numeric',
+			}),
+		];
+	};
 </script>
 
 <PageLayout>
-	<div class="text-right w-100">
-		{maxDownloads}
-	</div>
-	<div class="relative bg-dark p-4 rounded">
-		{#if hoveredPoint}
-			<span
-				class="absolute text-accent"
-				style="left: {mousePosition.x}px; top: {mousePosition.y + 20}px"
-			>
-				{hoveredPoint.DownloadCount}
+	<div class="bg-dark p-4 rounded text-sm">
+		<div class="flex gap-4">
+			<div class="flex flex-col justify-between text-right" style="line-height: 0">
+				<span>{maxDownloads}</span><span>{minDownloads}</span>
+			</div>
+			<div class="relative flex-1">
+				{#if hoveredPoint}
+					<span
+						class="absolute text-center bg-darker p-2 rounded min-w-max z-10"
+						style="left: {mousePosition.x}px; top: {mousePosition.y + 20}px"
+					>
+						<div class="text-accent">
+							{hoveredPoint.DownloadCount}
+						</div>
+						<div class="text-light">
+							{getDateText(hoveredPoint)}
+						</div>
+					</span>
+				{/if}
+				<svg
+					viewBox="0 0 500 100"
+					class="block"
+					on:mouseout={() => {
+						hoveredPoint = null;
+					}}
+					on:mousemove={(event) => {
+						const hoveredXRatio = event.offsetX / event.currentTarget.getBoundingClientRect().width;
+						mousePosition = {
+							x: event.offsetX,
+							y: event.offsetY,
+						};
+						const hoveredTimestamp =
+							firstPoint.UnixTimestamp +
+							hoveredXRatio * (lastPoint.UnixTimestamp - firstPoint.UnixTimestamp);
+						for (const historyPoint of modDownloadHistory) {
+							if (
+								!hoveredPoint ||
+								Math.abs(hoveredTimestamp - historyPoint.UnixTimestamp) <
+									Math.abs(hoveredTimestamp - hoveredPoint.UnixTimestamp)
+							) {
+								hoveredPoint = historyPoint;
+							}
+						}
+					}}
+					on:focus={() => {
+						/* todo */
+					}}
+					on:blur={() => {
+						/* todo */
+					}}
+				>
+					<polyline
+						fill="none"
+						class="stroke-accent opacity-80"
+						stroke-width="1"
+						points={modDownloadHistory
+							.map(
+								({ UnixTimestamp, DownloadCount }) =>
+									`${getX(UnixTimestamp)},${getY(DownloadCount)}`
+							)
+							.join(' ')}
+					/>
+					<line class="stroke-light opacity-80" stroke-width="1" x1="0" y1="100%" x2="0" y2="0" />
+					<line
+						class="stroke-light opacity-80"
+						stroke-width="1"
+						x1="0"
+						y1="100%"
+						x2="100%"
+						y2="100%"
+					/>
+					{#if hoveredPoint}
+						<circle
+							cy={getY(hoveredPoint.DownloadCount)}
+							cx={getX(hoveredPoint.UnixTimestamp)}
+							r={3}
+							class="fill-accent"
+						/>
+					{/if}
+				</svg>
+			</div>
+		</div>
+		<div class="mb-10">
+			<span class="rotate-45 translate-x-12 translate-y-6 inline-block">
+				{getDateText(firstPoint)}
 			</span>
-		{/if}
-		<svg
-			viewBox="0 0 500 100"
-			on:mousemove={(event) => {
-				const hoveredXRatio = event.offsetX / event.currentTarget.getBoundingClientRect().width;
-				mousePosition = {
-					x: event.offsetX,
-					y: event.offsetY,
-				};
-				const hoveredTimestamp =
-					firstPoint.UnixTimestamp +
-					hoveredXRatio * (lastPoint.UnixTimestamp - firstPoint.UnixTimestamp);
-				for (const historyPoint of modDownloadHistory) {
-					if (
-						!hoveredPoint ||
-						Math.abs(hoveredTimestamp - historyPoint.UnixTimestamp) <
-							Math.abs(hoveredTimestamp - hoveredPoint.UnixTimestamp)
-					) {
-						hoveredPoint = historyPoint;
-					}
-				}
-			}}
-			on:focus={() => {
-				/* todo */
-			}}
-		>
-			<polyline
-				fill="none"
-				class="stroke-accent opacity-80"
-				stroke-width="1"
-				points={modDownloadHistory
-					.map(
-						({ UnixTimestamp, DownloadCount }) => `${getX(UnixTimestamp)},${getY(DownloadCount)}`
-					)
-					.join(' ')}
-			/>
-			{#if hoveredPoint}
-				<circle
-					cy={getY(hoveredPoint.DownloadCount)}
-					cx={getX(hoveredPoint.UnixTimestamp)}
-					r={3}
-					class="fill-accent"
-				/>
-			{/if}
-		</svg>
-	</div>
-	<div>
-		<span>
-			{new Date(firstPoint.UnixTimestamp * 1000).toLocaleDateString()}
-		</span>
-		<span class="float-right">
-			{new Date(lastPoint.UnixTimestamp * 1000).toLocaleDateString()}
-		</span>
+			<span class="float-right rotate-45 translate-x-12 translate-y-6 inline-block">
+				{getDateText(lastPoint)}
+			</span>
+		</div>
 	</div>
 </PageLayout>
