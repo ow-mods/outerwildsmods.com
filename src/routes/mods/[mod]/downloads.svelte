3d1d4c11b0a6c0299753e11755701a8c45e89a1e
Ricardo <Raicuparta@users.noreply.github.com>
2022-04-04 12:00:35 +0200

    compare history

--- src/routes/mods/[mod]/downloads.svelte
+++ src/routes/mods/[mod]/downloads.svelte
@@ -1,6 +1,5 @@
 <script lang="ts" context="module">
 	import type { Load } from '@sveltejs/kit';
-	import { modList } from '$lib/store';
 
 	export const prerender = true;
 
@@ -53,15 +52,58 @@
 		HistoryPoint,
 	} from '$lib/components/downloads-chart/downloads-chart.svelte';
 	import { getModPathName } from '$lib/helpers/mod-path-name';
+	import { modList } from '$lib/store';
 
 	export let modDownloadHistory: HistoryPoint[] = [];
 	export let mod: ModsRequestItem;
+
+	let compareWithMod: ModsRequestItem | null = null;
+	let compareWithHistory: HistoryPoint[] = [];
+
+	const getCompareWithHistory = async (modToCompare: ModsRequestItem) => {
+		const modDownloadhistoryResponse = await fetch(
+			`/api/${modToCompare.author}/${getModRepoName(modToCompare)}/downloads.json`
+		);
+
+		if (modDownloadhistoryResponse.ok) {
+			return modDownloadhistoryResponse.json();
+		}
+	};
+
+	$: (async () => {
+		if (compareWithMod) {
+			const modDownloadhistoryResponse = await fetch(
+				`/api/${compareWithMod.author}/${getModRepoName(compareWithMod)}/downloads.json`
+			);
+
+			if (modDownloadhistoryResponse.ok) {
+				compareWithHistory = await modDownloadhistoryResponse.json();
+			}
+		}
+	})();
 </script>
 
 <PageLayout>
 	<div class="flex items-center gap-4 mb-4">
 		<LinkButton href=".." isSmall>‹ Back to {mod.name}</LinkButton>
 	</div>
+	<select
+		class="select"
+		on:change={(event) => {
+			compareWithMod = $modList.find((mod) => mod.uniqueName === event.currentTarget.value) || null;
+		}}
+	>
+		<option value={null}>None</option>
+		{#each $modList as mod}
+			<option value={mod.uniqueName}>{mod.name}</option>
+		{/each}
+	</select>
 	<PageSectionTitle id="downloads">{mod.name} downloads over time</PageSectionTitle>
 	<DownloadsChart historyPoints={modDownloadHistory} />
+	{#if compareWithHistory.length > 0}
+		<div>
+			Compare with {compareWithMod?.name}
+		</div>
+		<DownloadsChart historyPoints={compareWithHistory} />
+	{/if}
 </PageLayout>
