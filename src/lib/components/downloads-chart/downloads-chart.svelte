d10820b99fdc0ef53ad9e08c684507fbe6f6bf82
Raicuparta <raicuparta@gmail.com>
2022-04-04 19:04:45 +0200

    show compare values, dirty code

--- src/lib/components/downloads-chart/downloads-chart.svelte
+++ src/lib/components/downloads-chart/downloads-chart.svelte
@@ -71,6 +71,7 @@
 	let heightMuliplier = -chartSize.y / (maxDownloads - minDownloads);
 
 	let hoveredPoint: HistoryPoint | null = null;
+	let hoveredPointCompare: HistoryPoint | null = null;
 
 	let getX = (historyPoint: HistoryPoint) =>
 		(historyPoint.UnixTimestamp - firstPoint.UnixTimestamp) * widthMultiplier;
@@ -97,6 +98,7 @@
 		const hoveredTimestamp =
 			firstPoint.UnixTimestamp +
 			hoveredXRatio * (lastPoint.UnixTimestamp - firstPoint.UnixTimestamp);
+		hoveredPoint = null;
 		for (const historyPoint of historyPoints) {
 			if (
 				!hoveredPoint ||
@@ -104,6 +106,19 @@
 					Math.abs(hoveredTimestamp - hoveredPoint.UnixTimestamp)
 			) {
 				hoveredPoint = historyPoint;
+			}
+		}
+		hoveredPointCompare = null;
+		for (const comparePoint of comparePoints) {
+			const distanceToHovered = Math.abs(hoveredTimestamp - comparePoint.UnixTimestamp);
+			if (distanceToHovered * widthMultiplier > chartSize.x / 100) continue;
+
+			if (
+				!hoveredPointCompare ||
+				Math.abs(hoveredTimestamp - comparePoint.UnixTimestamp) <
+					Math.abs(hoveredTimestamp - hoveredPointCompare.UnixTimestamp)
+			) {
+				hoveredPointCompare = comparePoint;
 			}
 		}
 	};
@@ -148,8 +163,15 @@
 					style="left: {mousePosition.x + tooltipOffset.x}px; top: {mousePosition.y +
 						tooltipOffset.y}px"
 				>
-					<div class="text-accent">
-						{hoveredPoint.DownloadCount}
+					<div class="flex gap-2 justify-center">
+						<span class="text-accent">
+							{hoveredPoint.DownloadCount}
+						</span>
+						{#if hoveredPointCompare}
+							<span class="text-cta">
+								{hoveredPointCompare.DownloadCount}
+							</span>
+						{/if}
 					</div>
 					<div class="text-light">
 						{getDateText(hoveredPoint)}
@@ -191,7 +213,14 @@
 						y2="100%"
 					/>
 					{#if hoveredPoint}
-						<circle cy={getY(hoveredPoint)} cx={getX(hoveredPoint)} r={3} class="fill-accent" />
+						<line
+							y1={0}
+							y2="100%"
+							x2={getX(hoveredPoint)}
+							x1={getX(hoveredPoint)}
+							stroke-dasharray="5,5"
+							class="stroke-light"
+						/>
 					{/if}
 				</g>
 			</svg>
