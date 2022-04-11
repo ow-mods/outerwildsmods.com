8daf31843359a01d90e533c1263df6c9ffd5cd71
Raicuparta <raicuparta@gmail.com>
2022-04-11 18:36:15 +0200

    test prerendering concurrency

--- src/routes/api/[userName]/[repoName]/index.json.ts
+++ src/routes/api/[userName]/[repoName]/index.json.ts
@@ -1,15 +1,12 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { getRawContentUrl } from '$lib/helpers/get-raw-content-url';
 import { getModReadme } from '$lib/helpers/api/get-mod-readme';
-import { getAllMarkdownImages } from '$lib/helpers/api/get-markdown-images';
-import { getImageMap } from '$lib/helpers/api/get-image-map';
 
 export const get: RequestHandler = async ({ params }) => {
 	const { userName, repoName } = params;
 
 	const rawContentUrl = getRawContentUrl(`https://github.com/${userName}/${repoName}`);
 	const readme = await getModReadme(rawContentUrl);
-	const images = getAllMarkdownImages(readme);
 
 	return {
 		body: JSON.stringify({
