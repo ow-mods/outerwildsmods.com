696ef3608db9fee4a352739d1a2d3159d38244d4
Raicuparta <raicuparta@gmail.com>
2022-04-13 18:36:55 +0200

    ok no concurrency again

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 					`Failed to find path. status: ${status}, path: ${path}, referrer: ${referrer}, referenceType: ${referenceType}`
 				);
 			},
-			concurrency: 4,
+			concurrency: 1,
 		},
 		trailingSlash: 'always',
 		vite: {
