a64679b7c8d1177d59f40abebf7a56f76f161e67
Raicuparta <raicuparta@gmail.com>
2022-04-14 15:21:17 +0200

    only generate gifs for thumbnails

--- src/lib/helpers/api/get-optimized-image.ts
+++ src/lib/helpers/api/get-optimized-image.ts
@@ -60,7 +60,7 @@ export const getOptimizedImage = async (
 	const height = resizeHeight ?? imageMetadata.height ?? 0 * resizeRatio;
 	const format = imageMetadata.format;
 	const isSvg = format === 'svg';
-	const isAnimated = (imageMetadata.pages ?? 0) > 1;
+	const isAnimatedThumbnail = Boolean(resizeWidth) && (imageMetadata.pages ?? 0) > 1;
 
 	if (!width || !height) {
 		throw new Error('failed to read image dimensions');
@@ -101,7 +101,7 @@ export const getOptimizedImage = async (
 
 		let openGraphUrl = fullOptimizedImageUrl;
 
-		if (isAnimated) {
+		if (isAnimatedThumbnail) {
 			const openGraphFileName = getFileName('gif');
 			const openGraphImagePath = `${fileOutputDir}/${openGraphFileName}`;
 			openGraphUrl = `${optimizedDir}/${openGraphFileName}`;
