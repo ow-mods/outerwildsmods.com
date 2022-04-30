7733db3381e76e3eec9e5597cc368a93a1e98c61
Nick <nicholasconnors@hotmail.ca>
2022-04-30 13:47:56 -0400

    Put NH info back on custom worlds + docs links (#33)

--- src/routes/custom-worlds/index.svelte
+++ src/routes/custom-worlds/index.svelte
@@ -5,12 +5,34 @@
 	import PageLayout from '$lib/components/page-layout.svelte';
 	import PageSection from '$lib/components/page-section/page-section.svelte';
 	import { modList } from '$lib/store';
+	import LinkList from '$lib/components/link-list.svelte';
 
 	const newHorizons = $modList.find((mod) => mod.uniqueName === 'xen.NewHorizons');
+	const newHorizonsLinks = [
+		{
+			text: 'New Horizons Documentation',
+			href: 'https://nh.outerwildsmods.com/'
+		},
+		{
+			text: 'New Horizons Addon Template',
+			href: 'https://github.com/xen-42/ow-new-horizons-config-template'
+		}
+	];
 </script>
 
 <PageLayout>
 	{#if newHorizons}
+		<PageSection title="Custom Worlds" id="custom-worlds">
+			<div class="flex flex-col md:flex-row gap-8">
+				<ModCard mod={newHorizons} />
+				<div>
+					<p class="mt-0">
+						New Horizons is a world creation tool capable of creating new planets and star systems, and even editing planets from the base game. If you want to make your own custom worlds, the NH documentation has most of the info you need to get started. The easiest way is by cloning the NH Addon Template. No coding knowledge is required!			
+						<LinkList links={newHorizonsLinks} />
+					</p>
+				</div>
+			</div>
+		</PageSection>
 		<PageSection title="Available Custom Worlds" id="available-worlds">
 			<ModAddons mod={newHorizons} />
 		</PageSection>
