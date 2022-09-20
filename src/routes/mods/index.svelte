0cde90b9de2ce548a87f09d44a0766e98cddfa8e
Damián Garro <dgarro@dc.uba.ar>
2022-09-20 18:40:37 -0300

    Show addons of utility mods in standard mods list

--- src/routes/mods/index.svelte
+++ src/routes/mods/index.svelte
@@ -6,8 +6,9 @@
 	import { modList } from '$lib/store';
 	import SortedModGrid from '$lib/components/sorted-mod-grid.svelte';
 
-	const standardMods = $modList.filter((mod) => !mod.utility && !mod.parent && !mod.alpha);
 	const utilityMods = $modList.filter((mod) => mod.utility && !mod.parent && !mod.alpha);
+	const utilityModsUniqueNames = utilityMods.map((mod) => mod.uniqueName);
+	const standardMods = $modList.filter((mod) => !mod.utility && (!mod.parent || utilityModsUniqueNames.includes(mod.parent)) && !mod.alpha);
 </script>
 
 <svelte:head>
