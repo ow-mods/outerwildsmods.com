35b2d472b2869cea08f6c369ebc7bf438cdc4ab2
Raicuparta <Raicuparta@users.noreply.github.com>
2022-10-17 15:36:46 +0200

    update more dependencies

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,11 @@
 import type { ModsRequestItem } from '../routes/api/mods.json/+server';
 import { writable } from 'svelte/store';
-import type { OctokitAuthenticatedUser } from './octokit';
-import type { Octokit } from 'octokit';
 import type { DownloadHistory } from './helpers/api/get-download-history';
 import type { ModDatabase } from './helpers/api/get-mod-database';
 
 export const modList = writable<ModsRequestItem[]>([]);
 
 export const modDatabase = writable<ModDatabase | undefined>();
-
-export const githubUser = writable<OctokitAuthenticatedUser | undefined>();
-
-export const octokit = writable<Octokit | undefined>();
 
 export const downloadHistory = writable<DownloadHistory | undefined>([]);
 
