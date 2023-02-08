6477d5d9f1afc04c0cf9c05f097bc18aebe61666
Raicuparta <Raicuparta@users.noreply.github.com>
2023-02-08 13:24:30 +0100

    Jam credits (#64)
    * Add credits
    
    * add images to credits
    
    * mobile-friendly

---
+++ src/routes/jam/jam-credit-column.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { sortBy } from 'lodash-es';
+
+	export let people: Record<string, string>;
+	export let title: string;
+
+	const peopleList = sortBy(Object.entries(people), ([displayName]) => displayName.toUpperCase());
+	const avatarSize = 40;
+</script>
+
+<div class="flex flex-col gap-4">
+	<h3 class="mt-0 mb-2">{title}</h3>
+	{#each peopleList as [displayName, id]}
+		<a
+			class="link flex gap-2 items-center"
+			href={id.length === 0 || id.startsWith('http') ? id : `https://github.com/${id}`}
+		>
+			<img
+				alt={displayName}
+				src={id.length === 0 || id.startsWith('http')
+					? '/images/placeholder.webp'
+					: `https://github.com/${id}.png?size=${avatarSize}`}
+				width={avatarSize}
+				height={avatarSize}
+				class="rounded-full outline-3 outline outline-darker"
+			/>
+			{displayName}
+		</a>
+	{/each}
+</div>
---
+++ src/routes/jam/jam-credits.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import JamCreditColumn from './jam-credit-column.svelte';
+
+	const organizers = {
+		xen: 'xen-42',
+		Idiot: 'Bwc9876',
+		_nebula: 'misternebula',
+		JohnCorby: 'JohnCorby',
+		Raicuparta: 'Raicuparta',
+	};
+
+	const judges = {
+		Bird: '',
+		Book: 'Nageld',
+		Bunnie: 'BUNN1E5',
+		MegaPiggy: 'MegaPiggy',
+		Vesper: 'Vesper-Works',
+		_nebula: 'misternebula',
+		JohnCorby: 'JohnCorby',
+		xen: 'xen-42',
+	};
+
+	const donators = {
+		_nebula: 'misternebula',
+		Raicuparta: 'Raicuparta',
+		xen: 'xen-42',
+		Book: 'Nageld',
+		Pixie: 'https://www.reddit.com/user/littlemetalpixie',
+	};
+</script>
+
+<div class="flex flex-col justify-evenly items-center md:flex-row md:items-start gap-8">
+	<JamCreditColumn title="Organizers" people={organizers} />
+	<JamCreditColumn title="Judges" people={judges} />
+	<JamCreditColumn title="Donators" people={donators} />
+</div>
