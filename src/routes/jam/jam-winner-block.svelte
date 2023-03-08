f8d9192e1fc655e1952b15ea9e359979e40feace
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-08 14:28:54 +0100

    center jam winner text

--- src/routes/jam/jam-winner-block.svelte
+++ src/routes/jam/jam-winner-block.svelte
@@ -7,7 +7,7 @@
 	export let subtitle: string;
 </script>
 
-<div class="bg-darker p-2 rounded mb-4">
+<div class="bg-darker p-2 rounded mb-4 text-center">
 	<h3 class="m-0">{title}</h3>
 	<div class="mb-2">{subtitle}</div>
 	<div class="flex gap-2">
