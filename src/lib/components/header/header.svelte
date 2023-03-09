2a317bf781e498598baa7a8fe45b5d5b9cfe62f8
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-09 16:45:40 +0100

    start work on animated header

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -2,15 +2,31 @@
 	import GithubCorner from '$lib/components/header/github-corner.svelte';
 	import Navigation from '$lib/components/navigation/navigation.svelte';
 	import NavigationLink from '../navigation/navigation-link.svelte';
+
+	let video: HTMLVideoElement | undefined;
+	$: {
+		if (video) {
+			video.playbackRate = 0.3;
+		}
+	}
 </script>
 
-<header class="relative text-center overflow-hidden">
+<header class="text-center overflow-hidden">
+	<GithubCorner href="https://github.com/ow-mods" tooltip="Outer Wilds Mods ecosystem on GitHub" />
 	<div class="gradient">
-		<div class="background max-w-screen-lg m-auto">
-			<GithubCorner
-				href="https://github.com/ow-mods"
-				tooltip="Outer Wilds Mods ecosystem on GitHub"
-			/>
+		<div class="max-w-screen-lg m-auto relative background">
+			<div class="mix-blend-screen">
+				<div class="video-gradient absolute w-full h-full z-10" />
+				<video
+					autoplay
+					muted
+					loop
+					bind:this={video}
+					class="absolute w-full object-contain object-right h-full brightness-75"
+				>
+					<source src="/images/header-planet.mp4" type="video/mp4" />
+				</video>
+			</div>
 			<div class="p-4 text-4xl m-0">
 				<a class="text-white font-thin" href="/">Outer Wilds Mods</a>
 			</div>
@@ -40,5 +56,8 @@
 	}
 	.gradient {
 		background: radial-gradient(circle, transparent 30%, black 100%);
+	}
+	.video-gradient {
+		background: linear-gradient(90deg, rgba(0, 0, 0, 0) 80%, rgba(0, 0, 0, 1) 100%);
 	}
 </style>
