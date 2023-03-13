c90ebf4cbcf6141cc95c3754534f5899a03abeec
Raicuparta <Raicuparta@users.noreply.github.com>
2023-03-13 17:17:40 +0100

    show different message if linked from notification

--- src/lib/components/error-message.svelte
+++ src/lib/components/error-message.svelte
@@ -3,13 +3,31 @@
 	import PageSection from '$lib/components/page-section/page-section.svelte';
 	import PageSectionColumns from '$lib/components/page-section/page-section-columns.svelte';
 	import PageSectionDescription from '$lib/components/page-section/page-section-description.svelte';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	let linkedFromNotification = false;
+	onMount(() => {
+		linkedFromNotification = Boolean($page.url.searchParams.get('linked-from-notification'));
+	});
 </script>
 
-<PageSection id="not-found" title="Page not found">
-	<PageSectionColumns>
-		<PageSectionDescription
-			description="Sorry. Not sure what you were looking for but I didn't find it. Try using the navigation buttons at the top of the page to find what you want."
-		/>
-		<PageSectionImage title="Error" imageUrl="/images/error.png" />
-	</PageSectionColumns>
-</PageSection>
+{#if linkedFromNotification}
+	<PageSection id="not-ready" title="Page not ready yet">
+		<PageSectionColumns>
+			<PageSectionDescription
+				description="This page is still cooking. Come back in a few minutes!"
+			/>
+			<PageSectionImage title="Error" imageUrl="/images/cooking.jpg" />
+		</PageSectionColumns>
+	</PageSection>
+{:else}
+	<PageSection id="not-found" title="Page not found">
+		<PageSectionColumns>
+			<PageSectionDescription
+				description="Sorry. Not sure what you were looking for but I didn't find it. Try using the navigation buttons at the top of the page to find what you want."
+			/>
+			<PageSectionImage title="Error" imageUrl="/images/error.png" />
+		</PageSectionColumns>
+	</PageSection>
+{/if}
