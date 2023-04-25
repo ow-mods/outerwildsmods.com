64310a1f7250d04432e9a65bca4d8e3cfc4c7f1d
Damián Garro <dgarro@dc.uba.ar>
2023-04-24 21:41:23 -0300

    Make selected tags orange again (fix #66) (#67)

--- src/lib/components/tag-toggle.svelte
+++ src/lib/components/tag-toggle.svelte
@@ -4,8 +4,9 @@
 </script>
 
 <button
-	class="px-1 rounded bg-dark text-sm border-dark text-light"
+	class="px-1 rounded bg-dark text-sm border-dark"
 	class:text-accent={selected}
+	class:text-light={!selected}
 	class:opacity-75={!selected}
 	{title}
 	on:click
