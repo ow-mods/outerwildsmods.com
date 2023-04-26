cc9fdb677ac4d8bec2367ad9b4bb4e9eed632843
Ben C <bwc9876@gmail.com>
2023-04-26 17:20:44 -0400

    Make basic outline

---
+++ src/lib/components/icons/nix-icon.svelte
@@ -0,0 +1,72 @@
+<svg
+	class="fill-white mr-2"
+	width="22"
+	height="22"
+	viewBox="0 0 20.624999 20.625002"
+	id="svg2"
+	version="1.1"
+>
+	<g
+		id="layer3"
+		style="display:inline;opacity:1"
+		transform="matrix(0.04111905,0,0,0.04739851,-6.434462,44.238293)"
+	>
+		<path
+			id="path3336-6"
+			d="m 309.54892,-710.38827 122.19683,211.67512 -56.15706,0.5268 -32.6236,-56.8692 -32.85645,56.5653 -27.90237,-0.011 -14.29086,-24.6896 46.81047,-80.4901 -33.22946,-57.8257 z"
+			style="opacity:1;fill:currentColor;fill-opacity:1;fill-rule:evenodd;stroke:none;stroke-width:3;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+		/>
+		<use
+			height="100%"
+			width="100%"
+			transform="rotate(60,407.11155,-715.78724)"
+			id="use3439-6"
+			xlink:href="#path3336-6"
+			y="0"
+			x="0"
+		/>
+		<use
+			height="100%"
+			width="100%"
+			transform="rotate(-60,407.31177,-715.70016)"
+			id="use3445-0"
+			xlink:href="#path3336-6"
+			y="0"
+			x="0"
+		/>
+		<use
+			height="100%"
+			width="100%"
+			transform="rotate(180,407.41868,-715.7565)"
+			id="use3449-5"
+			xlink:href="#path3336-6"
+			y="0"
+			x="0"
+		/>
+		<path
+			style="clip-rule:nonzero;display:inline;overflow:visible;visibility:visible;opacity:1;isolation:auto;mix-blend-mode:normal;color-interpolation:sRGB;color-interpolation-filters:linearRGB;solid-color:#000000;solid-opacity:1;fill:currentColor;fill-opacity:1;fill-rule:evenodd;stroke:none;stroke-width:3;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;color-rendering:auto;image-rendering:auto;shape-rendering:auto;text-rendering:auto;enable-background:accumulate"
+			d="m 309.54892,-710.38827 122.19683,211.67512 -56.15706,0.5268 -32.6236,-56.8692 -32.85645,56.5653 -27.90237,-0.011 -14.29086,-24.6896 46.81047,-80.4901 -33.22946,-57.8256 z"
+			id="path4260-0"
+		/>
+		<use
+			height="100%"
+			width="100%"
+			transform="rotate(120,407.33916,-716.08356)"
+			id="use4354-5"
+			xlink:href="#path4260-0"
+			y="0"
+			x="0"
+			style="display:inline"
+		/>
+		<use
+			height="100%"
+			width="100%"
+			transform="rotate(-120,407.28823,-715.86995)"
+			id="use4362-2"
+			xlink:href="#path4260-0"
+			y="0"
+			x="0"
+			style="display:inline"
+		/>
+	</g>
+</svg>
--- src/routes/mod-manager/+page.svelte
+++ src/routes/mod-manager/+page.svelte
@@ -3,45 +3,104 @@
 	import PageContainer from '$lib/components/page-container.svelte';
 	import PageSectionImage from '$lib/components/page-section/page-section-image.svelte';
 	import PageSection from '$lib/components/page-section/page-section.svelte';
-	import WindowsIcon from '$lib/components/windows-icon.svelte';
+	import WindowsIcon from '$lib/components/icons/windows-icon.svelte';
 	import { websiteUrl } from '$lib/helpers/constants';
+	import DebianIcon from '$lib/components/icons/debian-icon.svelte';
+	import ArchIcon from '$lib/components/icons/arch-icon.svelte';
+	import AppImageIcon from '$lib/components/icons/app-image-icon.svelte';
+	// import NixIcon from '$lib/components/icons/nix-icon.svelte';
 
-	const repoUrl = 'https://github.com/ow-mods/ow-mod-manager';
-	const downloadUrl = `${repoUrl}/releases/latest/download`;
-	const installerDownloadUrl = `${downloadUrl}/OuterWildsModManager-Installer.exe`;
-	const portableDownloadUrl = `${downloadUrl}/OuterWildsModManager-Portable.zip`;
+	const repoUrl = 'https://github.com/Bwc9876/ow-mod-man';
+	const aurUrl = 'https://aur.archlinux.org/packages/owmods-gui-bin';
+	const managerVersion = '0.3.1';
+	const downloadUrl = `${repoUrl}/releases/download/gui_v${managerVersion}`;
+	const installerDownloadUrl = `${downloadUrl}/Outer.Wilds.Mod.Manager_${managerVersion}_x64_en-US.msi`;
+	const appImageUrl = `${downloadUrl}/outer-wilds-mod-manager_${managerVersion}_amd64.AppImage `;
+	const debUrl = `${downloadUrl}/outer-wilds-mod-manager_${managerVersion}_amd64.deb`;
+
+	const cliVersion = '0.3.1';
+	const cliAurUrl = 'https://aur.archlinux.org/packages/owmods-cli-bin';
+	const cliDownloadUrl = `${repoUrl}/release/download/cli_v${cliVersion}`;
+	const cliDebUrl = `${cliDownloadUrl}/owmods-cli_${cliVersion}_amd64.deb`;
+	const cliTarUrl = `${cliDownloadUrl}/owmods.tar.zst`;
 </script>
 
 <PageContainer
 	title="Outer Wilds Mod Manager - Download Windows app"
 	description="Download, install, and manage Outer Wilds mods using the Outer Wilds Mod Manager for Windows."
 	imageUrl="{websiteUrl}/images/mod-manager.webp"
-	imageWidth={887}
-	imageHeight={416}
+	imageWidth={777}
+	imageHeight={563}
 >
 	<PageSection title="Outer Wilds Mod Manager" id="mod-manager" isNarrow>
 		<div>
 			<PageSectionImage imageUrl="/images/mod-manager.webp" title="Outer Wilds Mod Manager" />
 		</div>
 		<div class="my-4">
-			<CtaButton href={installerDownloadUrl} rel="noopener noreferrer">
-				<WindowsIcon />
-				Download the Outer Wilds Mod Manager for Windows
-			</CtaButton>
+			<details open>
+				<summary>For Windows</summary>
+				<div class="mt-2">
+					<CtaButton href={installerDownloadUrl} rel="noopener noreferrer">
+						<WindowsIcon />
+						Download the Outer Wilds Mod Manager for Windows
+					</CtaButton>
+				</div>
+			</details>
 		</div>
-		<p>
-			or{' '}
-			<a class="link" href={portableDownloadUrl} rel="noopener noreferrer">
-				download the portable version instead
-			</a>
-		</p>
+		<div>
+			<details>
+				<summary>For Linux</summary>
+				<div class="my-2">
+					<CtaButton href={debUrl} rel="noopener noreferrer">
+						<DebianIcon />
+						Download .deb package for Debian
+					</CtaButton>
+				</div>
+				<div class="my-2">
+					<CtaButton href={aurUrl} rel="noopener noreferrer">
+						<ArchIcon />
+						AUR Package for Arch Linux
+					</CtaButton>
+				</div>
+				<!-- Waiting on Loco to make a Nix pkg for the GUI -->
+				<!-- <div class="my-2">
+					<CtaButton href={aurUrl} rel="noopener noreferrer">
+						<NixIcon />
+						NixOS Package
+					</CtaButton>
+				</div> -->
+				<div class="my-2">
+					<CtaButton href={appImageUrl} rel="noopener noreferrer">
+						<AppImageIcon />
+						Download AppImage For Linux
+					</CtaButton>
+				</div>
+				<p class="mt-1">
+					* Linux installs require that <a
+						href="https://www.mono-project.com/"
+						class="link"
+						target="_blank"
+						rel="noopener noreferrer">Mono</a
+					> is installed and available on the PATH
+				</p>
+			</details>
+		</div>
 		<p class="mt-4">For all your modding needs! With access to features such as:</p>
 		<ul class="my-4">
 			<li>Downloading mods;</li>
 			<li>Updating mods;</li>
 			<li>Enabling / disabling mods;</li>
-			<li>Looking at all the great mods you have;</li>
-			<li>Running the game with mods enabled.</li>
+			<li>Looking at all the great mods you have in several different themes;</li>
+			<li>Installing mods straight from the website;</li>
+			<li>Installing mods from URLs and ZIP files;</li>
+			<li>Running the game with mods enabled;</li>
+			<li>
+				Patching the game to run modded on Linux through <a
+					href="https://www.mono-project.com/"
+					class="link"
+					rel="noopener noreferrer">Mono</a
+				>.
+			</li>
 		</ul>
 		<p>
 			It also lets you install / update the{' '}
@@ -52,15 +111,12 @@
 	<PageSection title="How do I use this?" id="how-to-use" isNarrow>
 		<ul>
 			<li>Download the Outer Wilds Mod Manager installer;</li>
-			<li>Run the downloaded .exe (you might need to ignore some Chrome / Windows warnings);</li>
-			<li>The Mod Manager should start automatically;</li>
-			<li>
-				Shortcuts are added to desktop and start menu, use them next time you want to run the
-				manager;
-			</li>
+			<li>Run the downloaded .msi (you might need to ignore some Chrome / Windows warnings);</li>
+			<li>Shortcuts are added to desktop and start menu, use them to run the manager;</li>
+			<li>Run the manager</li>
 			<li>Install OWML;</li>
-			<li>Install any mods you want;</li>
-			<li>Press the big green button that says "Start Game";</li>
+			<li>Head to the "Get Mods" tab to install any mods you want;</li>
+			<li>Press the top center play button to launch the game;</li>
 			<li>You won't believe what happens next.</li>
 		</ul>
 	</PageSection>
@@ -74,15 +130,47 @@
 			(or in the control panel), and then finding Outer Wilds Mod Manager in the list.
 		</p>
 	</PageSection>
+	<PageSection title="I Hate GUIs!" id="cli" isNarrow>
+		<p>That's not very nice, but a CLI version of the manager is available</p>
+		<p>On Windows:</p>
+		<ul>
+			<li>
+				You'll need to compile it with <a
+					class="link"
+					rel="noopener noreferrer"
+					href="https://www.rust-lang.org/tools/install">Rust</a
+				>
+			</li>
+			<li>
+				After installing rust and cargo, run <code>cargo install owmods_cli</code>
+			</li>
+		</ul>
+		<p>On Linux:</p>
+		<ul>
+			<li>
+				You can install <a class="link" rel="noopener noreferrer" href={cliAurUrl}>from the AUR</a> if
+				you're on an Arch based distro
+			</li>
+			<li>
+				You can install it <a class="link" rel="noopener noreferrer" href={cliDebUrl}
+					>from a .deb file</a
+				> provided in the GitHub releases
+			</li>
+			<li>
+				You can <a class="link" rel="noopener noreferrer" href={cliTarUrl}
+					>get the binary in a .tar.zst file</a
+				> provided in the GitHub releases
+			</li>
+			<li>Or you could follow the steps for Windows to install it via cargo</li>
+		</ul>
+		<p>
+			Once installed, run <code>owmods help</code> for a list of available commands
+		</p>
+	</PageSection>
 	<PageSection title="More information" id="more-info" isNarrow>
 		<p>
 			For more information, check the{' '}
-			<a
-				class="link"
-				target="_blank"
-				rel="noopener noreferrer"
-				href="https://github.com/ow-mods/ow-mod-manager#readme"
-			>
+			<a class="link" target="_blank" rel="noopener noreferrer" href={`${repoUrl}#readme`}>
 				readme on GitHub
 			</a>
 		</p>
