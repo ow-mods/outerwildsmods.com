e9b24de929090c69503ba7bcab2ab68aef0b7aac
Raicuparta <Raicuparta@users.noreply.github.com>
2023-05-30 20:08:17 +0200

    nice easing

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -132,9 +132,12 @@
 		getPositionInTimeline(date);
 
 	const scrollToEvent = (eventIndex: number) => {
-		document
-			.getElementById(`event-${eventIndex}`)
-			?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
+		const element = document.getElementById(`event-${eventIndex}`);
+		if (!element) return;
+
+		scrollTo(getPositionInTimeline(events[eventIndex].date), 1500, 'easeInOutCubic', () =>
+			console.log('done')
+		);
 	};
 
 	const selectPreviousEvent = () => {
@@ -156,6 +159,81 @@
 	onMount(() => {
 		selectEvent(1);
 	});
+
+	const easing = {
+		linear: function (t: number) {
+			return t;
+		},
+		easeInQuad: function (t: number) {
+			return t * t;
+		},
+		easeOutQuad: function (t: number) {
+			return t * (2 - t);
+		},
+		easeInOutQuad: function (t: number) {
+			return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+		},
+		easeInCubic: function (t: number) {
+			return t * t * t;
+		},
+		easeOutCubic: function (t: number) {
+			return --t * t * t + 1;
+		},
+		easeInOutCubic: function (t: number) {
+			return 0.5 * (Math.sin((t - 0.5) * Math.PI) + 1);
+		},
+		easeInQuart: function (t: number) {
+			return t * t * t * t;
+		},
+		easeOutQuart: function (t: number) {
+			return 1 - --t * t * t * t;
+		},
+		easeInOutQuart: function (t: number) {
+			return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
+		},
+		easeInQuint: function (t: number) {
+			return t * t * t * t * t;
+		},
+		easeOutQuint: function (t: number) {
+			return 1 + --t * t * t * t * t;
+		},
+		easeInOutQuint: function (t: number) {
+			return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
+		},
+	} as const;
+
+	type Easing = keyof typeof easing;
+
+	function scrollTo(x: number, duration: number, easingFunction: Easing, callback: () => void) {
+		var start = Date.now();
+		const elem = document.getElementById('piupi');
+		if (!elem) return;
+		const from = elem.scrollLeft;
+		const to = x - elem.clientWidth / 2;
+
+		if (from === to) {
+			callback();
+			return; /* Prevent scrolling to the Y point if already there */
+		}
+
+		function min(a: number, b: number) {
+			return a < b ? a : b;
+		}
+
+		function scroll(timestamp: number) {
+			if (!elem) return;
+			var currentTime = Date.now(),
+				time = min(1, (currentTime - start) / duration),
+				easedT = easing[easingFunction](time);
+
+			elem.scrollLeft = easedT * (to - from) + from;
+
+			if (time < 1) requestAnimationFrame(scroll);
+			else if (callback) callback();
+		}
+
+		requestAnimationFrame(scroll);
+	}
 </script>
 
 <div class="m-4">
@@ -165,7 +243,7 @@
 		<span>Selected: {selectedEvent}</span>
 		<span>Revealed: {revealedEvent}</span>
 	</div>
-	<div class="overflow-hidden my-20">
+	<div class="overflow-hidden my-20" id="piupi">
 		<div class="relative h-40">
 			<div class="pb-8 pt-4">
 				{#each months as month}
@@ -180,7 +258,7 @@
 			{#each events as event, index}
 				<div
 					id="event-{index}"
-					class="slow-transition absolute z-10 link"
+					class="slow-transition transition-delay absolute z-10 link"
 					class:opacity-0={index > revealedEvent}
 					style="left: {getPositionInTimeline(event.date)}px"
 					on:click={() => selectEvent(index)}
@@ -188,13 +266,16 @@
 				>
 					<div class="flex flex-col m-2 whitespace-nowrap relative">
 						<span
-							class="bg-white w-6 h-6 rounded-full slow-transition"
+							class="bg-white w-6 h-6 rounded-full slow-transition transition-delay"
 							class:scale-50={selectedEvent !== index}
 						/>
 						<span
-							class="rotate-12 top-6 left-1 absolute w-0 text-white slow-transition"
-							class:opacity-50={selectedEvent !== index}>{event.title}</span
+							class="rotate-12 top-6 left-1 absolute w-0 text-white slow-transition transition-delay"
+							class:opacity-50={selectedEvent !== index}
+							class:scale-50={revealedEvent < index}
 						>
+							{event.title}
+						</span>
 					</div>
 				</div>
 			{/each}
@@ -202,7 +283,7 @@
 				style="min-width: {timelineWidth + timelineMargin * 2}px; left: {getPositionInTimeline(
 					events[revealedEvent].date
 				)}px"
-				class="bg-accent h-2 m-4 rounded absolute gaga fast-transition"
+				class="bg-accent h-2 m-4 rounded absolute gaga slow-transition"
 			/>
 		</div>
 	</div>
@@ -211,10 +292,10 @@
 <style>
 	.slow-transition {
 		transition: 1.5s;
-		transition-delay: 0.3s;
+		transition-timing-function: ease-in-out;
 	}
-	.fast-transition {
-		transition: 0.8s;
+	.transition-delay {
+		transition-delay: 1.3s;
 	}
 	.gaga {
 		transform: translate(-100%, 0px);
