73cb23ed21b32c5506709972604c2341ab30f03a
Raicuparta <Raicuparta@users.noreply.github.com>
2023-06-21 03:54:49 +0200

    Add RSS (#75)
    * add rss
    
    * don't provide headers, to make sure it's static
    
    * this thing I guess? I dunno

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,3 +1,8 @@
 <p class="text-center text-sm m-8">
-	This page isn't official, nor affiliated with Mobius Digital, or anyone really.
+	This page isn't official, nor affiliated with Mobius Digital, or anyone really. <a
+		class="link"
+		href="/feed.xml"
+	>
+		RSS
+	</a>
 </p>
