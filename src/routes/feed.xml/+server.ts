bd0b98d4863dd16d319c38a8eb1c280bd44c640c
Locochoco <ivanrwpf@gmail.com>
2023-06-22 22:46:07 -0300

    added channel of daily releases

--- src/routes/feed.xml/+server.ts
+++ src/routes/feed.xml/+server.ts
@@ -28,38 +28,64 @@ function escapeXml(unsafe: string) {
 	});
 }
 
+function splitModsByReleaseDate(mods: Mod[]) {
+	const mapOfMods: Map<string,Mod[]> = new Map();
+  mods.forEach((mod: Mod) => { 
+    const releaseDate: Date = new Date(mod.firstReleaseDate);
+    const key: string = releaseDate.toDateString();
+    if(!mapOfMods.has(key)){
+  	  mapOfMods.set(key, []);
+    }  
+    mapOfMods.get(key).push(mod);
+    return mapOfMods;
+  });
+  //console.log(mapOfMods);
+  return [... mapOfMods.values()];
+}
+
+function dailyModsRelease(mods: Mod[]) {
+
+	const releaseDate: Date = new Date(mods[0].firstReleaseDate);
+  const date: string = releaseDate.toDateString();
+	return `
+    <item>
+      <title>Mods Released in ${escapeXml(date)}</title>
+      <guid>${escapeXml(date)}</guid>
+      <pubDate>${escapeXml(date)}</pubDate>
+      <link>mods/?sortOrder=newest</link>
+      <description>
+      <![CDATA[
+      <ul>
+      ${mods
+      .map((mod)=>`
+      	<li>
+      	<a href="${mod.repo}">${escapeXml(mod.name)}</a> &#10145; ${escapeXml(mod.description)}
+        ${
+					mod.thumbnail.main
+						? `<br></br>
+           <img src="${escapeXml(`${thumbnailUrlBase}/${mod.thumbnail.main}`)}">`
+						: ''
+				}</li>`).join('')
+      }
+      </ul>
+      ]]>
+      </description>
+    </item>
+  `;
+}
+
 const xml = (mods: Mod[]) => `<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-   <title>Outer Wilds Mods</title>
-   <description>List of mods for Outer Wilds</description>
+   <title>Outer Wilds Mods Daily Releases</title>
+   <description>List of daily releases of mods for Outer Wilds</description>
    <link>${escapeXml(websiteUrl)}</link>
    <atom:link href="${escapeXml(websiteUrl)}/feed.xml"
      rel="self" type="application/rss+xml" />
   <image>${escapeXml(websiteUrl)}/images/icon-large.png</image>
-${mods
-	.map(
-		(mod) => `
-    <item>
-      <title>${escapeXml(mod.name)}</title>
-      <link>${escapeXml(websiteUrl)}/mods/${escapeXml(mod.slug)}</link>
-      <guid>${escapeXml(mod.uniqueName)}</guid>
-      <pubDate>${escapeXml(mod.firstReleaseDate)}</pubDate>
-      <description>
-        ${escapeXml(mod.description)}
-        ${
-					mod.thumbnail.main
-						? `<![CDATA[
-           <br></br>
-           <img src="${escapeXml(`${thumbnailUrlBase}/${mod.thumbnail.main}`)}">
-        ]]>`
-						: ''
-				}
-      </description>
-    </item>
-`
-	)
-	.join('')}
+${splitModsByReleaseDate(mods).
+	map((splitMods: Mod[]) =>
+	dailyModsRelease(splitMods)).join('')}
 </channel>
 </rss>
 `;
