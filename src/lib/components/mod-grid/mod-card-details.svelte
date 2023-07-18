7afb7f073fd82643d81a26b5c260f6e58493f052
Raicuparta <Raicuparta@users.noreply.github.com>
2023-07-18 10:51:53 +0200

    Add install button to mod card

--- src/lib/components/mod-grid/mod-card-details.svelte
+++ src/lib/components/mod-grid/mod-card-details.svelte
@@ -1,14 +1,32 @@
 <script lang="ts">
 	import type { Mod } from '$lib/helpers/api/get-mod-list';
+	import { onMount } from 'svelte/internal';
 
-	export let mod: Pick<Mod, 'name'>;
+	export let mod: Mod;
+
+	let ready = false;
+
+	onMount(() => {
+		ready = true;
+	});
 </script>
 
-<div class="p-3">
-	<span class="text-accent text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block">
-		{mod.name}
-	</span>
-	<div class="text-light text-xs font-light">
-		<slot />
+<div class="flex flex-1">
+	<div class="p-3 flex-1">
+		<span class="text-accent text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block">
+			{mod.name}
+		</span>
+		<div class="text-light text-xs font-light">
+			<slot />
+		</div>
 	</div>
+	<button
+		disabled={!ready}
+		title="Install {mod.name}"
+		class="p-2 flex items-center text-light hover:text-white hover:bg-dark bg-darker"
+		on:click|preventDefault={() =>
+			(window.location.href = `owmods://install-mod/${mod.uniqueName}`)}
+	>
+		🠋
+	</button>
 </div>
