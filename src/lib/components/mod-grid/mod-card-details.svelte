ccffcc833615008b3f0b1da5829f75053b0f05d6
Raicuparta <Raicuparta@users.noreply.github.com>
2023-07-18 11:02:52 +0200

    Swap mod info for installing message

--- src/lib/components/mod-grid/mod-card-details.svelte
+++ src/lib/components/mod-grid/mod-card-details.svelte
@@ -5,6 +5,7 @@
 	export let mod: Mod;
 
 	let ready = false;
+	let clickedInstall = false;
 
 	onMount(() => {
 		ready = true;
@@ -13,19 +14,31 @@
 
 <div class="flex flex-1">
 	<div class="p-3 flex-1">
-		<span class="text-accent text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block">
-			{mod.name}
-		</span>
-		<div class="text-light text-xs font-light">
-			<slot />
-		</div>
+		{#if clickedInstall}
+			<div class="text-xs text-light flex flex-col gap-2">
+				<div>Installing {mod.name}...</div>
+				<div>
+					If nothing happens, <a class="link" href="/mod-manager">download the Manager</a> and open it
+					at least once, then try again.
+				</div>
+			</div>
+		{:else}
+			<span class="text-accent text-sm whitespace-nowrap overflow-hidden overflow-ellipsis block">
+				{mod.name}
+			</span>
+			<div class="text-light text-xs font-light">
+				<slot />
+			</div>
+		{/if}
 	</div>
 	<button
 		disabled={!ready}
 		title="Install {mod.name}"
 		class="p-2 flex items-center text-light hover:text-white hover:bg-dark bg-darker"
-		on:click|preventDefault={() =>
-			(window.location.href = `owmods://install-mod/${mod.uniqueName}`)}
+		on:click|preventDefault={() => {
+			clickedInstall = true;
+			window.location.href = `owmods://install-mod/${mod.uniqueName}`;
+		}}
 	>
 		🠋
 	</button>
