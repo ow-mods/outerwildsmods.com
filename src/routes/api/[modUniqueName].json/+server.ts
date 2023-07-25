7864c59cf2e2cb27a8cc6922c0016e292783c325
Ben C <bwc9876@gmail.com>
2023-07-25 10:48:16 -0400

    idk update stuff

--- src/routes/api/[modUniqueName].json/+server.ts
+++ src/routes/api/[modUniqueName].json/+server.ts
@@ -1,30 +1,28 @@
-import { error, type RequestHandler } from '@sveltejs/kit';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 import { getModReadme } from '$lib/helpers/api/get-mod-readme';
 import { getImageMap } from '$lib/helpers/api/get-image-map';
 import type { Mod } from '$lib/helpers/api/get-mod-list';
 
 type Params = {
-	modUniqueName: string;
+    modUniqueName: string;
 };
 
 export const GET: RequestHandler<Params> = async ({ params, fetch }) => {
-	const { modUniqueName } = params;
+    const { modUniqueName } = params;
 
-	const modList = (await (await fetch('/api/mods.json')).json()) as Mod[];
+    const modList = (await (await fetch('/api/mods.json')).json()) as Mod[];
 
-	const mod = modList.find(({ uniqueName }) => uniqueName === modUniqueName);
+    const mod = modList.find(({ uniqueName }) => uniqueName === modUniqueName);
 
-	if (!mod) {
-		throw error(500, `Failed to find mod ${modUniqueName}`);
-	}
+    if (!mod) {
+        throw error(500, `Failed to find mod ${modUniqueName}`);
+    }
 
-	const readme = await getModReadme(mod);
-	const imageMap = readme ? await getImageMap(mod, readme) : {};
+    const readme = await getModReadme(mod);
+    const imageMap = readme ? await getImageMap(mod, readme) : {};
 
-	return new Response(
-		JSON.stringify({
-			...(readme ? { readme } : undefined),
-			imageMap,
-		})
-	);
+    return json({
+        ...(readme ? { readme } : undefined),
+        imageMap,
+    });
 };
--- src/routes/mods/[mod]/+page.ts
+++ src/routes/mods/[mod]/+page.ts
@@ -11,7 +11,7 @@ export const load: PageLoad = async ({ fetch, params, parent }) => {
         throw error(404, `Could not find mod ${params.mod}.`);
     }
 
-    // Unused fetch to get the badge endpoint to prerender because sveltekit can't comprehend basic SSG :)
+    // Intentionally unused.
     await fetch(`/api/badges/${mod.uniqueName}.json`);
 
     const result = await fetch(`/api/${mod.uniqueName}.json`);
