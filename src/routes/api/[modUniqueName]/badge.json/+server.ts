032729e76063dbc2dcc4c30effc98e8a48582a53
Ben C <bwc9876@gmail.com>
2023-07-25 10:09:03 -0400

    idk at this point

--- src/routes/api/[modUniqueName]/badge.json/+server.ts
+++ src/routes/api/[modUniqueName]/badge.json/+server.ts
@@ -8,11 +8,6 @@ type Params = {
 
 export const prerender = true;
 
-export async function entries() {
-    const modList = (await (await fetch('/api/mods.json')).json()) as Mod[];
-    return modList.map(({ uniqueName }) => ({ modUniqueName: uniqueName }));
-}
-
 export const GET: RequestHandler<Params> = async ({ params, fetch }) => {
     const { modUniqueName } = params;
 
