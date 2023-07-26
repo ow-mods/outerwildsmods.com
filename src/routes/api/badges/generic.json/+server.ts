5086ad2096ef3d7ddbd5ef1f1f3f5d3c07c19f3d
Ben C <bwc9876@gmail.com>
2023-07-25 23:05:23 -0400

    Fix

--- src/routes/api/badges/generic.json/+server.ts
+++ src/routes/api/badges/generic.json/+server.ts
@@ -1,6 +1,8 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import { BASE_BADGE, type ShieldsEndpointBadge } from '$lib/helpers/api/shields';
 
+export const prerender = true;
+
 export const GET: RequestHandler = async () => {
 	const badgeInfo: Partial<ShieldsEndpointBadge> = {
 		...BASE_BADGE,
