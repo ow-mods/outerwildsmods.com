b4e3c63de26b4afda51811fceceb5b4c29a2dae8
Raicuparta <Raicuparta@users.noreply.github.com>
2023-07-27 03:05:54 +0200

    prevent selecting text on clicking code snippet

--- src/lib/components/code-snippet.svelte
+++ src/lib/components/code-snippet.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
 	const copyElementText = ({ currentTarget }: { currentTarget: HTMLElement }) => {
-		window.getSelection()?.selectAllChildren(currentTarget);
 		navigator.clipboard.writeText(currentTarget.innerText);
+		clicked = true;
+		setTimeout(() => (clicked = false), 500);
 	};
 
+	export let clicked = false;
 	export let title = '';
 </script>
 
 <div>
 	<div class="flex justify-between items-end">
 		<span>{title}</span>
-		<span class="text-xs opacity-50">(click text to copy)</span>
+		<span class="text-xs opacity-50" class:opacity-100={clicked}>(click text to copy)</span>
 	</div>
 	<code
-		class="text-xs text-light opacity-60 bg-darker p-1 rounded leading-none cursor-pointer break-words block whitespace-pre-wrap"
+		class="text-xs text-light bg-darker opacity-60 p-1 rounded cursor-pointer break-words block whitespace-pre-wrap"
 		title="Mod install badge"
 		on:click={copyElementText}
-		on:keypress={copyElementText}
+		on:keydown={copyElementText}
 	>
 		<slot />
 	</code>
