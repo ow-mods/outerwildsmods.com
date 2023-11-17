d6bacd0cffe6b8ac64184396c12c650cedfe81a1
Raicuparta <Raicuparta@users.noreply.github.com>
2023-11-17 13:22:11 +0100

    try to make sharp external but i dont think it works

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: ["sharp"]
+		},
+	},
+	ssr: {
+		external: ["sharp"]
+	}
 };
 
 export default config;
