5a16a380e943fb6d14b3566074ae18fd64ca266f
Raicuparta <Raicuparta@users.noreply.github.com>
2023-11-17 12:45:52 +0100

    Update to svelte 4, other dependencies, accessibility improvements

--- src/lib/components/code-snippet.svelte
+++ src/lib/components/code-snippet.svelte
@@ -14,12 +14,12 @@
 		<span>{title}</span>
 		<span class="text-xs opacity-50" class:opacity-100={clicked}>(click text to copy)</span>
 	</div>
-	<code
-		class="text-xs text-light bg-darker opacity-60 p-1 rounded cursor-pointer break-words block whitespace-pre-wrap"
-		title="Mod install badge"
-		on:click={copyElementText}
-		on:keydown={copyElementText}
-	>
-		<slot />
-	</code>
+	<button on:click={copyElementText} on:keydown={copyElementText}>
+		<code
+			class="text-xs text-light bg-darker opacity-60 p-1 rounded cursor-pointer break-words block whitespace-pre-wrap"
+			title="Mod install badge"
+		>
+			<slot />
+		</code>
+	</button>
 </div>
