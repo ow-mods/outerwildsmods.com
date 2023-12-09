2591c2dfeec6b3de2d10c3e894bb0b6d530cb228
Raicuparta <Raicuparta@users.noreply.github.com>
2023-12-10 00:01:26 +0100

    reduce comments layout shifts

--- src/lib/components/comments.svelte
+++ src/lib/components/comments.svelte
@@ -45,35 +45,39 @@
 </script>
 
 <PageSection title="Comments" id={commentsSectionId}>
-	<div
-		class:hidden={isLoaded}
-		class="bg-dark p-4 rounded h-64 flex flex-col items-center justify-center"
-	>
-		<div class="animate-pulse text-4xl">🔥</div>
-		<div>Loading comments...</div>
-		<div class:invisible={isVisible} class="text-sm">(Javascript needs to be enabled for this)</div>
-	</div>
-	<div bind:this={container}>
-		<div class:invisible={!isLoaded} class:absolute={!isLoaded}>
-			{#if isVisible}
-				<script
-					src="https://giscus.app/client.js"
-					data-repo="ow-mods/outerwildsmods.com"
-					data-repo-id="R_kgDOGfoiNQ"
-					data-category="Comments"
-					data-category-id="DIC_kwDOGfoiNc4CabnK"
-					data-mapping="specific"
-					data-term={id}
-					data-theme="{$page.url.origin}/styles/giscus.css"
-					data-strict="1"
-					data-reactions-enabled="0"
-					data-emit-metadata="1"
-					data-input-position="top"
-					data-lang="en"
-					crossorigin="anonymous"
-				>
-				</script>
-			{/if}
+	<div class="min-h-[16rem]">
+		<div
+			class:hidden={isLoaded}
+			class="bg-dark p-4 rounded h-64 flex flex-col items-center justify-center"
+		>
+			<div class="animate-pulse text-4xl">🔥</div>
+			<div>Loading comments...</div>
+			<div class:invisible={isVisible} class="text-sm">
+				(Javascript needs to be enabled for this)
+			</div>
+		</div>
+		<div bind:this={container}>
+			<div class:invisible={!isLoaded} class:absolute={!isLoaded}>
+				{#if isVisible}
+					<script
+						src="https://giscus.app/client.js"
+						data-repo="ow-mods/outerwildsmods.com"
+						data-repo-id="R_kgDOGfoiNQ"
+						data-category="Comments"
+						data-category-id="DIC_kwDOGfoiNc4CabnK"
+						data-mapping="specific"
+						data-term={id}
+						data-theme="{$page.url.origin}/styles/giscus.css"
+						data-strict="1"
+						data-reactions-enabled="0"
+						data-emit-metadata="1"
+						data-input-position="top"
+						data-lang="en"
+						crossorigin="anonymous"
+					>
+					</script>
+				{/if}
+			</div>
 		</div>
 	</div>
 </PageSection>
