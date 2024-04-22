ce2d89ab5c5eb4a9c47a447fd5261d953f2f8f66
Raicuparta <Raicuparta@users.noreply.github.com>
2024-04-22 10:47:00 +0200

    clickable section titles

--- src/lib/components/page-section/page-section-title.svelte
+++ src/lib/components/page-section/page-section-title.svelte
@@ -4,8 +4,8 @@
 	export let id: string;
 </script>
 
-<div class="flex mb-4 w-full items-center">
+<a class="flex mb-4 w-full items-center" href="#{id}">
 	<HorizontalLine />
 	<h2 class="text-2xl my-0 m-4 text-center" {id}><slot /></h2>
 	<HorizontalLine />
-</div>
+</a>
