9e7455b232a34f9925d0f2957e07edff02e51c07
Raicuparta <Raicuparta@users.noreply.github.com>
2025-05-24 16:16:10 +0200

    use a thousand new remark dependencies instead

--- src/lib/helpers/api/get-mod-readme.ts
+++ src/lib/helpers/api/get-mod-readme.ts
@@ -1,6 +1,24 @@
 import { getRawContentUrl } from '../get-raw-content-url';
 import type { ModFromDatabase } from './get-mod-database';
-import showdown from 'showdown';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
+import { visit } from 'unist-util-visit';
+
+// TODO fix these types
+function remarkFixRelativeLinks(baseUrl: string) {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	return (tree: any) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		visit(tree, ['link', 'image'], (node: any) => {
+			if (node.url && !node.url.startsWith('http') && !node.url.startsWith('#')) {
+				node.url = `${baseUrl.replace(/\/$/, '')}/${node.url.replace(/^\.\//, '')}`;
+			}
+		});
+	};
+}
 
 export const getModReadme = async (mod: ModFromDatabase): Promise<string | null> => {
 	if (!mod.readme) return null;
@@ -15,29 +33,22 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 
 	const markdown = await response.text();
 	const rawContentUrl = getRawContentUrl(mod);
-	const converter = new showdown.Converter({
-		ghCompatibleHeaderId: true,
-		ghMentions: true,
-	});
-	converter.addExtension(
-		{
-			type: 'output',
-			filter: (text: string) => {
-				if (rawContentUrl) {
-					return text.replace(/(href|src)="([^"]+)"/g, (match, attr, val) => {
-						if (!val.startsWith('http') && !val.startsWith('#')) {
-							return `${attr}="${rawContentUrl}/${val}"`;
-						}
-						return match;
-					});
-				}
-				return text;
-			},
-		},
-		'relative-links'
-	);
 
-	converter.setFlavor('github');
+	const file = await unified()
+		.use(remarkParse)
+		.use(remarkGfm)
+		// TODO fix these types
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error
+		.use(remarkFixRelativeLinks, rawContentUrl)
+		.use(remarkRehype, {
+			allowDangerousHtml: true,
+		})
+		.use(rehypeStringify, {
+			allowDangerousHtml: true,
+		})
+		.process(markdown);
+	file.toString();
 
-	return converter.makeHtml(markdown);
+	return file.toString();
 };
