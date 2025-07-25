e2eeaed79905a7e744bd2283f5d6f51432ed87b0
Raicuparta <Raicuparta@users.noreply.github.com>
2025-07-25 23:10:11 +0200

    Table of contents again (#291)

---
+++ src/lib/components/heading-list.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { HeadingStructure } from '$lib/helpers/api/get-mod-readme';
+
+	export let headings: HeadingStructure[];
+</script>
+
+{#if headings.length > 0}
+	<ul class="list-disc list-outside space-y-1 pl-4">
+		{#each headings as heading}
+			<li>
+				<a href="#{heading.slug}" class="link hover:text-accent" title={heading.text}>
+					{heading.text}
+				</a>
+				{#if heading.children.length > 0}
+					<svelte:self headings={heading.children} />
+				{/if}
+			</li>
+		{/each}
+	</ul>
+{/if}
--- src/lib/helpers/api/get-mod-readme.ts
+++ src/lib/helpers/api/get-mod-readme.ts
@@ -9,8 +9,66 @@ import rehypeSanitize from 'rehype-sanitize';
 import { unified } from 'unified';
 import { getImageInfo, ImageInfo } from './get-image-info';
 import { visit } from 'unist-util-visit';
+import { toString } from 'mdast-util-to-string';
+import GithubSlugger from 'github-slugger';
 
-export const getModReadme = async (mod: ModFromDatabase): Promise<string | null> => {
+export interface HeadingStructure {
+	text: string;
+	slug: string;
+	level: number;
+	children: HeadingStructure[];
+}
+
+export interface ModReadmeResult {
+	html: string;
+	headings: HeadingStructure[];
+}
+
+function buildHeadingStructure(
+	headings: Array<{ text: string; level: number }>
+): HeadingStructure[] {
+	const result: HeadingStructure[] = [];
+	const stack: HeadingStructure[] = [];
+
+	// This is what rehype-slug uses internally.
+	// Important to use the same thing to keep TOC and readme titles in sync.
+	const slugger = new GithubSlugger();
+
+	for (const heading of headings) {
+		const slug = slugger.slug(heading.text);
+		const headingNode: HeadingStructure = {
+			text: heading.text,
+			slug,
+			level: heading.level,
+			children: [],
+		};
+
+		while (stack.length > 0 && stack[stack.length - 1].level >= heading.level) {
+			stack.pop();
+		}
+
+		if (stack.length === 0) {
+			result.push(headingNode);
+		} else {
+			stack[stack.length - 1].children.push(headingNode);
+		}
+
+		stack.push(headingNode);
+	}
+
+	if (result.length === 1 && result[0].children.length > 0) {
+		// Special case for when there's a top level heading that contains everything else.
+		// In this case we flatten the top level to be in line with its direct children,
+		// too avoid too much nesting. We don't have much horizontal space here.
+		const topLevelHeading = result[0];
+		const flattenedResult = [{ ...topLevelHeading, children: [] }, ...topLevelHeading.children];
+		return flattenedResult;
+	}
+
+	return result;
+}
+
+export const getModReadme = async (mod: ModFromDatabase): Promise<ModReadmeResult | null> => {
 	if (!mod.readme) return null;
 
 	const response = await fetch(mod.readme.downloadUrl);
@@ -23,9 +81,11 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 
 	const markdown = await response.text();
 
+	const markdownAst = unified().use(remarkParse).parse(markdown);
+
 	const imageSources: string[] = [];
 
-	visit(unified().use(remarkParse).parse(markdown), 'image', (node) => {
+	visit(markdownAst, 'image', (node) => {
 		if (typeof node.url === 'string') {
 			imageSources.push(node.url);
 		}
@@ -45,6 +105,8 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 		})
 	);
 
+	const headings: Array<{ text: string; level: number }> = [];
+
 	const file = await unified()
 		.use(remarkParse)
 		.use(remarkGfm)
@@ -63,8 +125,6 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 					) {
 						const original = element.properties.href;
 						element.properties.href = `${mod.repo}/blob/HEAD/${original.replace(/^\.\//, '')}`;
-
-						console.log(`replaced link href: ${original} >> ${element.properties.href}`);
 					}
 					return element;
 				},
@@ -79,6 +139,17 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 					element.properties.src = imageInfos[src]?.url;
 					return element;
 				},
+				heading: (state, node) => {
+					if (node.children?.[0]?.type === 'text') {
+						const text = toString(node);
+						headings.push({
+							text,
+							level: node.depth,
+						});
+					}
+
+					return defaultHandlers.heading(state, node);
+				},
 			},
 		})
 		.use(rehypeRaw)
@@ -86,7 +157,11 @@ export const getModReadme = async (mod: ModFromDatabase): Promise<string | null>
 		.use(rehypeSlug)
 		.use(rehypeStringify)
 		.process(markdown);
-	file.toString();
 
-	return file.toString();
+	const headingStructure = buildHeadingStructure(headings);
+
+	return {
+		html: file.toString(),
+		headings: headingStructure,
+	};
 };
