d45081588a8bf590b467148d9c91288f263e5aea
Raicuparta <Raicuparta@users.noreply.github.com>
2025-09-17 23:58:59 +0200

    update eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,68 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+
+const tsParser = require('@typescript-eslint/parser');
+const svelte = require('eslint-plugin-svelte');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const parser = require('svelte-eslint-parser');
+const globals = require('globals');
+const js = require('@eslint/js');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+	{
+		languageOptions: {
+			parser: tsParser,
+			sourceType: 'module',
+			ecmaVersion: 2020,
+			parserOptions: {},
+
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+
+		extends: compat.extends(
+			'eslint:recommended',
+			'plugin:@typescript-eslint/recommended',
+			'prettier'
+		),
+
+		plugins: {
+			svelte,
+			'@typescript-eslint': typescriptEslint,
+		},
+	},
+	globalIgnores(['**/*.cjs']),
+	{
+		files: ['**/*.svelte'],
+
+		languageOptions: {
+			parser: parser,
+
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	},
+	globalIgnores([
+		'**/.DS_Store',
+		'**/node_modules',
+		'build',
+		'.svelte-kit',
+		'package',
+		'**/.env',
+		'**/.env.*',
+		'!**/.env.example',
+		'**/pnpm-lock.yaml',
+		'**/package-lock.json',
+		'**/yarn.lock',
+	]),
+]);
